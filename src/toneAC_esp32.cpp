331342486e6ac2655729ace3b86a9a778e657a68
regisdixit <regisdixit@localhost>
2023-03-26 19:02:29 -0400

    Signed-off-by: regisdixit <regisdixit@localhost>

--- src/toneAC_esp32.cpp
+++ src/toneAC_esp32.cpp
@@ -64,6 +64,7 @@ void toneAC_playNote(unsigned long frequency, uint8_t volume) {
 	mcpwm_start(MCPWM_UNIT_0, MCPWM_TIMER_0);
 }
 
+
 void toneAC_playWAV(unsigned char* data, unsigned long size, unsigned long resonant_freq, unsigned long rate, uint8_t background) {
 
 	if (_pcm_playing) return;
